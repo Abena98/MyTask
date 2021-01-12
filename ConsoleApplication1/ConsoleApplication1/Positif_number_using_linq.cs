6b7ab46b93fbd7743eeea68806345319436cf45f
79826 <79826@LAPTOP-1IAVPO8L>
2021-01-13 04:51:01 +0500

    My task 01 and 02  Variant 1

---
+++ ConsoleApplication1/ConsoleApplication1/Positif_number_using_linq.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // data source
+            int[] numbers = { 1, 2, -8, 4, 10, -6, 7, 8 };
+
+            //execute methode
+            GetpositifNumber(numbers);
+            
+            //keyboard run
+
+            Console.ReadKey();
+        }
+        static void GetpositifNumber(int[] array)
+        {
+            //// query expression
+            var numQuery =
+            from num in array
+            where (num > 0)
+            select num;
+            // query execution in foreach
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0} ", num);
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // data source
+            int[] numbers = { 1, 2, -8, 4, 10, -6, 7, 8 };
+            // query expression
+            var numQuery =
+            from num in numbers
+            where (num > 0)
+            select num;
+            // query execution in foreach
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0} ", num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
