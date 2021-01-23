27e572e65611bc1056163c47c60e692d94ca4fd2
79826 <79826@LAPTOP-1IAVPO8L>
2021-01-23 17:10:40 +0500

    positif number

---
+++ ConsoleApplication1/ConsoleApplication1/Positif_number_using_linq.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, -8, 10, 7, -6, 8 };
+            var result = GetpositifNumber(numbers);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        static char[] GetpositifNumber(int[] array)
+        {
+
+            return array.Where(item => item >= 0)
+                   .SelectMany(item => item.ToString().ToCharArray())
+                   .ToArray();
+        }
+    }
+}
